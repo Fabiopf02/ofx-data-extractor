cb961f2fa7580af76b710549a263961896446df8
Fábio Pereira França <fabiofranca593@gmail.com>
2023-08-07 10:11:04 -0300

    build: generation of umd and esm

--- rollup.config.js
+++ rollup.config.js
@@ -2,37 +2,50 @@ const { minify } = require('rollup-plugin-esbuild')
 const typescript = require('@rollup/plugin-typescript')
 const commonjs = require('@rollup/plugin-commonjs')
 const nodeResolve = require('@rollup/plugin-node-resolve')
-const name = require('./package.json').main.replace(/\.js$/, '')
+const lib = require('./package.json')
 
 /**
  * @type {import('rollup').RollupOptions}
  */
-module.exports = [
-  {
-    input: 'src/index.ts',
-    treeshake: true,
-    plugins: [
-      typescript({ tsconfig: './tsconfig.json' }),
-      commonjs(),
-      nodeResolve(),
-      minify({
-        keepNames: false,
-        treeShaking: true,
-        minify: true,
-      }),
-    ],
-    output: [
-      {
-        file: `${name}.js`,
-        format: 'cjs',
-        sourcemap: false,
+module.exports = () => {
+  const year = new Date().getFullYear()
+  const banner = `// Ofx-data-extractor v${lib.version} Copyright (c) ${year} ${lib.author}`
+
+  return [
+    {
+      input: 'src/index.ts',
+      treeshake: true,
+      plugins: [
+        typescript({ declaration: false }),
+        commonjs(),
+        nodeResolve(),
+        minify({
+          keepNames: false,
+          treeShaking: true,
+          minify: true,
+          banner,
+          globalName: 'ofx',
+        }),
+      ],
+      output: [
+        {
+          file: 'dist/umd/bundle.min.js',
+          format: 'umd',
+          name: 'ofx',
+        },
+        {
+          file: 'dist/esm/index.min.js',
+          format: 'esm',
+        },
+      ],
+    },
+    {
+      input: 'src/index.ts',
+      plugins: [typescript({ tsconfig: './tsconfig.json' })],
+      output: {
+        file: 'dist/index.d.ts',
+        format: 'esm',
       },
-      {
-        file: `${name}.js`,
-        format: 'iife',
-        name: 'ofx',
-        sourcemap: false,
-      },
-    ],
-  },
-]
+    },
+  ]
+}
